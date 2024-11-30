88d413950c2f5bdabe5e3f38b9077b4ca4b0037e
KiattisakDeeprasert <6531503199@lamduan.mfu.ac.th>
2024-11-30 21:12:53 +0700

    backend: setup backend

---
+++ backend/src/app/config/configuration.ts
@@ -0,0 +1,29 @@
+export default () => ({
+    port: parseInt(process.env.PORT, 10) || 8081,
+    baseUrl: process.env.BASE_URL || "http://127.0.0.1",
+    upload: {
+      baseUrl: process.env.UPLOAD_BASE_URL || "http://127.0.0.1:8081/uploads",
+      apiPath: process.env.UPLOAD_API_PATH || "/uploads",
+    },
+    mongodb: {
+      uri: process.env.MONGO_URI || "mongodb+srv://admin:user1234@cluster0.35bsc.mongodb.net/",
+      dbName: process.env.DATABASE_NAME || "cafe_management-db",
+    },
+    // database: process.env.MONGODB_URI,
+    // accessToken: {
+    //   secret: process.env.ACCESS_TOKEN_SECRET || "secret",
+    //   expiresIn: process.env.ACCESS_TOKEN_EXPIRES || "30m",
+    // },
+  
+    accessToken: {
+      secret: process.env.JWT_SECRET || "cafemanagement",
+      expiresIn: process.env.ACCESS_TOKEN_EXPIRES || "3d",
+    },
+    refreshToken: {
+      secret: process.env.REFRESH_TOKEN_SECRET || "secret",
+      expiresIn: process.env.REFRESH_TOKEN_EXPIRES || "3d",
+    },
+    bcrypt: {
+      saltOrRounds: Number(process.env.BCRYPT_SALT_OR_ROUNDS) || 10,
+    },
+  });
